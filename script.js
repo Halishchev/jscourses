bd331fb3f14915e927ca5dde0162850efed35f3a
Halishchev <work.andriihalishchev@gmail.com>
2022-08-28 16:12:44 +0300

    homework
    task

---
+++ script.js
@@ -0,0 +1,58 @@
+// task 1
+const horrors1 = 40;
+const horrors2 = 12;
+const horrors3 = 7;
+
+const adventures1 = 13;
+const adventures2 = 17;
+const adventures3 = 88;
+
+const others1 = 167;
+const others2 = 423;
+const others3 = 0;
+
+console.log(
+  horrors1 + horrors2 + horrors3,
+  adventures1 + adventures2 + adventures3,
+  others1 + others2 + others3
+);
+
+// task 2
+
+const name = "John";
+const admin = name;
+alert(admin);
+
+//task 3
+const nameOurPlanet = "";
+const currentUserName = "";
+
+//task 4
+let redBasketApple = 6;
+let redBasketBanana = 5;
+let redBasketBlueberry = 19;
+
+let blueBasketApple = 10;
+let blueBasketBanana = 1;
+let blueBasketBlueberry = 32;
+
+let brownBasketApple = 5;
+let brownBasketBanana = 10;
+let brownBasketBlueberry = 7;
+
+redBasketApple - 3 + 4,
+  redBasketBanana - 3,
+  redBasketBlueberry + 23,
+  brownBasketApple + 3 - 1,
+  brownBasketBanana + 3 - 5,
+  blueBasketApple - 4 + 1,
+  blueBasketBlueberry - 23,
+  blueBasketBanana + 5;
+
+console.log(
+  redBasketApple > blueBasketApple,
+  brownBasketBanana < redBasketBanana,
+  blueBasketBlueberry + redBasketBlueberry > brownBasketBlueberry,
+  blueBasketBanana + brownBasketBanana + redBasketBanana >
+    blueBasketBlueberry + brownBasketBlueberry + redBasketBlueberry
+);
